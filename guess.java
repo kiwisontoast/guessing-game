f298da5e9fc2c0ade97839e58c7b3293efbfaffd
kiwisontoast <devbshroff@gmail.com>
2022-09-05 22:44:56 -0500

    progress

--- guess.java
+++ guess.java
@@ -3,7 +3,9 @@ public class guess
 {
 	public static void main(String args[])
 	{
-		
+		int randomNum=0;
+        randomNum=(int)(Math.random()*100)+1;
+        System.out.print(randomNum);
 		
 	}
 }
